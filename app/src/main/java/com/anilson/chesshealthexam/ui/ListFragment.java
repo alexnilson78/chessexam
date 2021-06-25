84772e8c619221b7ceae9b417cbfc2054b3bad62
Alex Nilson <alex.nilson@metova.com>
2021-06-25 14:20:52 -0500

    Made the list views cards and adjusted padding

--- app/src/main/java/com/anilson/chesshealthexam/ui/ListFragment.java
+++ app/src/main/java/com/anilson/chesshealthexam/ui/ListFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
