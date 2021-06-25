78377b91067d98c19e0063979735c74641b1cd72
Alex Nilson <alex.nilson@metova.com>
2021-06-25 07:10:53 -0500

    Some cleanup on the add person dialog

--- app/src/main/java/com/anilson/chesshealthexam/ui/AddPersonDialog.java
+++ app/src/main/java/com/anilson/chesshealthexam/ui/AddPersonDialog.java
@@ -11,9 +11,12 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 public class AddPersonDialog extends BottomSheetDialogFragment {
@@ -38,6 +41,36 @@ public class AddPersonDialog extends BottomSheetDialogFragment {
             viewModel = new ViewModelProvider(getActivity()).get(PersonListViewModel.class);
         }
 
+        setTextWatcherForClearingError();
+        setImeActionHandler();
+        focusTextEntryAndShowKeyboard();
+        setSubmitClickListener();
+    }
+
+    private void setSubmitClickListener() {
+        binding.nameEntrySubmitButton.setOnClickListener(v -> {
+            String name = binding.nameEntryEditText.getText().toString();
+            if (name.isEmpty()) {
+                binding.errorIndicator.setVisibility(View.VISIBLE);
+            } else {
+                viewModel.addPerson(name);
+                closeKeyboard();
+                dismiss();
+            }
+        });
+    }
+
+    private void setImeActionHandler() {
+        binding.nameEntryEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                binding.nameEntrySubmitButton.performClick();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void setTextWatcherForClearingError() {
         binding.nameEntryEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -54,15 +87,24 @@ public class AddPersonDialog extends BottomSheetDialogFragment {
                 //NO OP
             }
         });
+    }
 
-        binding.nameEntrySubmitButton.setOnClickListener(v -> {
-            String name = binding.nameEntryEditText.getText().toString();
-            if (name.isEmpty()) {
-                binding.errorIndicator.setVisibility(View.VISIBLE);
-            } else {
-                viewModel.addPerson(name);
-                dismiss();
+    private void focusTextEntryAndShowKeyboard() {
+        if (getContext() != null) {
+            InputMethodManager inputMethodManager = ContextCompat.getSystemService(getContext(), InputMethodManager.class);
+            if (inputMethodManager!= null) {
+                inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                binding.nameEntryEditText.requestFocus();
             }
-        });
+        }
+    }
+
+    private void closeKeyboard() {
+        if (getContext() != null) {
+            InputMethodManager inputMethodManager = ContextCompat.getSystemService(getContext(), InputMethodManager.class);
+            if (inputMethodManager != null) {
+                inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+            }
+        }
     }
 }
