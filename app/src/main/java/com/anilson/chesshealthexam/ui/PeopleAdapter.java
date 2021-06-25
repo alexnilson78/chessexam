ad9b873b69d12ec9d6dc9b10831354401fbea578
Alex Nilson <alex.nilson@metova.com>
2021-06-25 07:51:19 -0500

    Format strings for adapter items

--- app/src/main/java/com/anilson/chesshealthexam/ui/PeopleAdapter.java
+++ app/src/main/java/com/anilson/chesshealthexam/ui/PeopleAdapter.java
@@ -1,17 +1,20 @@
 package com.anilson.chesshealthexam.ui;
 
+import com.anilson.chesshealthexam.R;
 import com.anilson.chesshealthexam.databinding.ItemPersonBinding;
 import com.anilson.chesshealthexam.db.entities.Person;
 
 import org.jetbrains.annotations.NotNull;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.text.NumberFormat;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder> {
@@ -37,10 +40,10 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
         NumberFormat percentFormat = NumberFormat.getPercentInstance();
         percentFormat.setMinimumFractionDigits(2);
         holder.binding.nameTextView.setText(person.name);
-        holder.binding.ageTextView.setText(String.valueOf(person.age));
-        holder.binding.genderTextView.setText(person.gender);
+        holder.binding.ageTextView.setText(getString(holder, R.string.age_format, person.age));
+        holder.binding.genderTextView.setText(getString(holder, R.string.gender_format, person.gender));
         holder.binding.genderProbabilityTextView.setText(percentFormat.format(person.genderProbability));
-        holder.binding.nationalityTextView.setText(person.countryCode);
+        holder.binding.nationalityTextView.setText(getString(holder, R.string.country_code_format, person.countryCode));
         holder.binding.nationalityProbability.setText(percentFormat.format(person.countryProbability));
         holder.binding.getRoot().setOnClickListener(v -> {
             callback.onListItemClick(person);
@@ -64,6 +67,10 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
 
     public Person getItemAt(int position) {
         return dataSet.get(position);
+    }
+
+    private String getString(ViewHolder holder, @StringRes int res, Object... args) {
+        return holder.binding.getRoot().getContext().getString(res, args);
     }
 
     public interface Callback {
