01bcda7f9b61b0be3ed2d1a33ff98e53945f0fde
Alex Nilson <alex.nilson@metova.com>
2021-06-25 06:51:08 -0500

    Searching functionality wired up

--- app/src/main/java/com/anilson/chesshealthexam/ui/MainActivity.java
+++ app/src/main/java/com/anilson/chesshealthexam/ui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.anilson.chesshealthexam.ui;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint;
 
 import com.anilson.chesshealthexam.R;
 import com.anilson.chesshealthexam.databinding.ActivityMainBinding;
+import com.anilson.chesshealthexam.ui.viewmodels.PersonListViewModel;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +23,7 @@ import android.view.MenuItem;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
+    private PersonListViewModel viewModel;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -31,28 +36,31 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+        viewModel = new ViewModelProvider(this).get(PersonListViewModel.class);
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        if (Intent.ACTION_SEARCH.equals(getIntent().getAction())) {
+            handleSearchIntent(getIntent());
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else  if (id == R.id.action_search) {
+            onSearchRequested();
         }
 
         return super.onOptionsItemSelected(item);
@@ -63,5 +71,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            handleSearchIntent(intent);
+        } else {
+            super.onNewIntent(intent);
+        }
+    }
+
+    private void handleSearchIntent(Intent intent) {
+        String query = intent.getStringExtra(SearchManager.QUERY);
+        viewModel.searchForPersonByName(query);
     }
 }
